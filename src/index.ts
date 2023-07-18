dcdd0c64b4128a28e4cdcc7dcd451530ca20a6bb
Manik <manik@pixelconception.com>
2023-07-18 11:58:39 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,19 @@
-let post: {
+type Post = {
   author: string;
   content: string;
   title: string;
   image?: string;
-} = {
+};
+
+type ArrayString = string[];
+
+let post: Post = {
+  author: "Stella",
+  content: "Hello World",
+  title: "My First Article",
+};
+
+let blog: Post = {
   author: "Stella",
   content: "Hello World",
   title: "My First Article",
