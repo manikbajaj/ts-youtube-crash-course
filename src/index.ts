be43fbedd424263bcf512751d2ddedad090886b3
Manik <manik@pixelconception.com>
2023-07-18 10:50:29 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,7 @@
-let booleanValue: boolean = true;
+let number: number = 10;
+number = -28.7;
 
-// Literal values
-booleanValue = false;
+number = Infinity * 2;
+number = parseInt("12");
 
-// valid expressions of true or false
-booleanValue = typeof "abc" == "string";
-booleanValue = 1 > 0;
-
-// You will get errors here
-booleanValue = {};
-booleanValue = "New York";
-
-// Undefined and Null are not considered by boolean values
-// they are separate types in typescript
-// though they are used as boolean values when you use them in javascript and would return false
-booleanValue = null;
-booleanValue = undefined;
+number = "1223";
