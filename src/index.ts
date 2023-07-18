3eb3cf768380fd809fb4c970e657bbaebd77e32a
Manik <manik@pixelconception.com>
2023-07-18 10:46:23 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,18 @@
-// unknown
-function mutiplyByTwo(number: unknown) {
-  if (typeof number === "number") {
-    return number * 2;
-  }
-}
+let booleanValue: boolean = true;
+
+// Literal values
+booleanValue = false;
+
+// valid expressions of true or false
+booleanValue = typeof "abc" == "string";
+booleanValue = 1 > 0;
+
+// You will get errors here
+booleanValue = {};
+booleanValue = "New York";
+
+// Undefined and Null are not considered by boolean values
+// they are separate types in typescript
+// though they are used as boolean values when you use them in javascript and would return false
+booleanValue = null;
+booleanValue = undefined;
